29f2a6abc310507a7c21ffa2b784e3c55e388cd6
Joao Gomes <joaopfg.2002@gmail.com>
2021-12-11 03:25:46 +0000

    Create impar validation and ensure that should be able to return list of impar numbers only

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,29 @@
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
+  def call(list), do: impar(list);
 
-  @doc """
-  Hello world.
+  defp check_number(list) do
+    list
+    |> Enum.flat_map(fn elem ->
+      case Integer.parse(elem) do
+        # transform to integer
+        {int, _rest} -> [int]
+        # skip the value
+        :error -> []
+      end
+    end)
 
-  ## Examples
-
-      iex> ListFilter.hello()
-      :world
-
-  """
-  def hello do
-    :world
   end
+
+  defp check_impar(list) do
+    list
+    |> Enum.filter(fn x -> if (x == 1 || rem(x, 2) == 1) do x end  end)
+
+  end
+
+  defp impar(list) do
+    list
+    |> check_number()
+    |> check_impar()
+  end
+
 end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,16 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
+  describe "call/1" do
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+    test "i should be able to return list of impar numbers" do
+      list = ["ola", "2", "1", "19", "12"]
+
+      response = ListFilter.call(list)
+
+      expected_response = [1, 19]
+
+      assert response == expected_response
+    end
+
   end
 end
